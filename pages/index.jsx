df77660726bb8b50dd5a17b654bae42f87b9d4e4
Eric Echemane <eechemane29@gmail.com>
2021-11-28 12:49:11 +0800

    Assets added

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+export default function Home() {
+  return (
+    <div>
+      <Box p={2}>
+        <Typography variant='h2'> Mascota de Cafe </Typography>
+        <Button> Hello </Button>
+      </Box>
+    </div>
+  );
+}
