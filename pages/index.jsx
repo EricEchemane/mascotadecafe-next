9ea2baf1b648c2dbf592bbd86e57c614699c366e
Eric Echemane <eechemane29@gmail.com>
2021-11-28 15:42:11 +0800

    getStaticProps implemented in index route to retrive coffees and pastries data

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,37 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Navbar from '../comps/Navbar';
+import { useContext, useEffect } from 'react';
+import { ShoppingCartContext } from '../context_hooks/ShoppingCartContext';
+import isDevMode from '../lib/node_env';
 
-export default function Home() {
+export default function Home({ coffees, pastries }) {
+
+  const {
+    cartItems,
+    totalPrice,
+    addToShoppingCart,
+    setCartItems } = useContext(ShoppingCartContext);
+
+  useEffect(() => {
+    const hasItems = localStorage.getItem('mascota-shopping-cart');
+    if (hasItems) setCartItems(JSON.parse(hasItems));
+  }, []);
+
   return <>
     <Navbar />
   </>;
+}
+
+export async function getStaticProps(context) {
+  const origin = isDevMode() ? 'http://localhost:3000' : '';
+  const coffeeRes = await fetch(`${origin}/data/coffee.json`);
+  const pastriesRes = await fetch(`${origin}/data/pastries.json`);
+
+  return {
+    props: {
+      coffees: await coffeeRes.json(),
+      pastries: await pastriesRes.json(),
+    }
+  };
 }
