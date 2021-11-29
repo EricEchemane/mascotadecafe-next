a3397e33e9ffbd922d72566a5662be7665cfae3d
Eric Echemane <eechemane29@gmail.com>
2021-11-29 09:28:55 +0800

    Added Link for coffee as index route and pastries as /pastry route

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,10 @@ import { useContext } from 'react';
 import { ShoppingCartContext } from '../context_hooks/ShoppingCartContext';
 import isDevMode from '../lib/node_env';
 import Image from 'next/image';
+import Link from 'next/link';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import CoffeeProductCard from '../comps/CoffeeProductCard';
 
 export default function Home({ coffees }) {
 
@@ -21,16 +24,18 @@ export default function Home({ coffees }) {
 
   const belowSmallDevices = useMediaQuery('(max-width: 950px)');
   const MD = useMediaQuery('(max-width: 800px)');
+  const XS = useMediaQuery('(max-width: 600px)');
 
   return <>
     <Navbar />
-    <Box className="darken-bg">
+    <Box className="darken-bg" mb="2rem">
 
       <Grid container className='cont' p="1.5rem">
 
         <Grid item xs={12} md={5}>
           <Typography
             className='md-center'
+            mt="1rem"
             variant={belowSmallDevices ? 'h4' : 'h3'}>
             Let&apos;s share
           </Typography>
@@ -53,18 +58,34 @@ export default function Home({ coffees }) {
             src='/assets/stories/s4.jpg'
             alt="Table with coffee and pastries"
             width={100}
-            height={60}
+            height={70}
             layout="responsive"
-            priority
+            priority={true}
             className='rounded'
           />
         </Grid>
       </Grid>
     </Box>
+
+    <Box p={XS ? '.5rem' : '1.5rem'} className='cont'>
+
+      <ButtonGroup variant="outlined" aria-label="products">
+        <Link href='/' passHref>
+          <Button> Coffee </Button>
+        </Link>
+        <Link href='/pastries' passHref>
+          <Button> Pastries </Button>
+        </Link>
+      </ButtonGroup>
+
+      <Grid container mt="1.5rem">
+        {coffees.map(coffee => <CoffeeProductCard {...coffee} key={coffee.id} />)}
+      </Grid>
+    </Box>
   </>;
 }
 
-export async function getStaticProps(context) {
+export async function getStaticProps() {
   const origin = isDevMode() ? 'http://localhost:3000' : 'https://mascotadecafe.vercel.app';
   const coffeeRes = await fetch(`${origin}/data/coffee.json`);
   return {
